f5869f363e1579b46b9ea7b8787d24da872ce788
Altin Morina <altinmorina2002@gmail.com>
2024-01-18 23:06:36 +0100

    Initial commit

---
+++ src/main/java/com/jobapplication/jobapplicationsystem/JobApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jobapplication.jobapplicationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobapplication/jobapplicationsystem/JobApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobapplication.jobapplicationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
